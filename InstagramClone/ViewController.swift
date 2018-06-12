5e1a5072eb964e7b6f1cdf84795d0af4c9f02fd5
stringdoth <31211425+stringdoth@users.noreply.github.com>
2018-06-12 14:41:52 +0530

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Bharath  Raj kumar on 12/06/18.
+//  Copyright © 2018 Pixl Ecosystems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
